cb63cd7a0534839546bb992380aa62bd34773704
Divyum <divyum@Divyums-MacBook-Pro.local>
2015-10-14 03:17:29 +0530

    adding temporary link display in dashboard -- time to sleep

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,15 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
   def index
+    @links = Link.where(:user_id => current_user)
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @links }
+    end
+  end
+
+  def show
+
   end
 end
