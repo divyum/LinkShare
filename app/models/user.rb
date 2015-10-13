65cd4b4e7949e0a88fd715197cc94675edc9cd24
Divyum <divyum@Divyums-MacBook-Pro.local>
2015-10-14 02:12:07 +0530

    making version changes and reimplementing login

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, :uniqueness => true, :presence => true, length: { in: 5..20 }
+  validates_format_of :username, with: /\A[a-zA-Z0-9]+\z/
+
+  attr_accessor :login
+
+  # overwite for user login
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+end
